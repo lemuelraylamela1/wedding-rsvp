1e9a4eeb792ecd9067a752a7f57292836fe3dd16
lemuelraylamela1 <lemuelraylamela@gmail.com>
2025-12-29 10:21:50 +0800

    minor fixes

--- src/app/components/EntourageImageSection.tsx
+++ src/app/components/EntourageImageSection.tsx
@@ -33,7 +33,7 @@ export function EntourageImageSection({
           }}
           className="relative w-full max-w-5xl mx-auto rounded-3xl overflow-hidden shadow-2xl cursor-pointer">
           <Image
-            src="/assets/entourage-list3.png"
+            src="/assets/Entourage-list3.png"
             alt="Entourage list"
             width={1200}
             height={800}
